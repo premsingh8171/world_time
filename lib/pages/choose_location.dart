f472a8a2b1cf6ee9d45b4e643017811c5aa37de4
premsingh8171 <premsingh8171@gmail.com>
2025-05-30 19:50:52 +0530

    used list view builder

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:world_time/services/world_time.dart';
 
 class ChooseLocation extends StatefulWidget {
   const ChooseLocation({super.key});
@@ -9,6 +10,15 @@ class ChooseLocation extends StatefulWidget {
 
 class _ChooseLocationState extends State<ChooseLocation> {
   int counter = 0;
+
+  List<WorldTime> locations = [
+    WorldTime(location: 'India1', flag: 'img3.jpg', url: 'Asia/India'),
+    WorldTime(location: 'India2', flag: 'img4.jpg', url: 'Asia/India'),
+    WorldTime(location: 'India3', flag: 'img3.jpg', url: 'Asia/India'),
+    WorldTime(location: 'India4', flag: 'img4.jpg', url: 'Asia/India'),
+    WorldTime(location: 'India5', flag: 'img3.jpg', url: 'Asia/India'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     print('build function run');
@@ -20,14 +30,25 @@ class _ChooseLocationState extends State<ChooseLocation> {
         centerTitle: true,
         elevation: 0,
       ),
-      body: ElevatedButton(
-        onPressed: () {
-          setState(() {
-            counter += 1;
-          });
+      body: ListView.builder(
+        itemCount: locations.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
+            child: Card(
+              child: ListTile(
+                onTap: () {
+                  print(locations[index].location);
+                },
+                title: Text(locations[index].location),
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage('assets/${locations[index].flag}'),
+                ),
+              ),
+            ),
+          );
         },
-        child: Text('counter is $counter '),
-      )
+      ),
     );
   }
 }
