824b6d6e1db6d17ee601c320287bd0e7e28dc596
premsingh8171 <premsingh8171@gmail.com>
2025-05-30 16:31:45 +0530

    world time custom class network call - api

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -9,13 +9,6 @@ class ChooseLocation extends StatefulWidget {
 
 class _ChooseLocationState extends State<ChooseLocation> {
   int counter = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    print('initState function ran');
-  }
-
   @override
   Widget build(BuildContext context) {
     print('build function run');
---
+++ lib/services/world_time.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class WorldTime{
+
+  late String location;
+  late String time;
+  late String flag;
+  late String url;
+
+  WorldTime({required this.location, required this.flag, required this.url});
+
+
+
+  Future<void> getTime() async {
+    var url = Uri.parse('https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLjQVOcTH0OSo8i_0VXfKAo9voQ_cfHm9gD5AOIiROUT9eVR_t9NE7SFlQPz6d5nHNZ58C5WRlxsP_K0H_1Xn4tG8hEec1B6EKD-47PY-iciQgc9ORmelfzvT5kBZFr0nmGd8E3N24_dgpL32WfVDqIjJhqxzJsOpwltmp6jaTpeP_jgiHjslx0isrIjg8ZEUGjSqIB4RoMyWNX4daNYPbTYy6Wo7gaOk6sAmYkg5WGqd6giJkeoQpn11P5Eo9dkwLgmg2r6&lib=MwxUjRcLr2qLlnVOLh12wSNkqcO1Ikdrk');
+    http.Response response = await http.get(url);
+    Map data =  jsonDecode(response.body);
+    print('$data');
+
+    String dayofweekName= data['dayofweekName'];
+    time = dayofweekName;
+  }
+
+  //http://worldtimeapi.org/api/timezone/Europe/Berlin
+}
