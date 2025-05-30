8750cb2c3bd1216ccf848de60082a45e6c86b4fc
premsingh8171 <premsingh8171@gmail.com>
2025-05-30 18:43:45 +0530

    Added Spinkit library for loading screen

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'dart:convert';
 import 'package:world_time/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -11,21 +11,6 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  /*  void getTime() async {
-    var url = Uri.parse('https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLjQVOcTH0OSo8i_0VXfKAo9voQ_cfHm9gD5AOIiROUT9eVR_t9NE7SFlQPz6d5nHNZ58C5WRlxsP_K0H_1Xn4tG8hEec1B6EKD-47PY-iciQgc9ORmelfzvT5kBZFr0nmGd8E3N24_dgpL32WfVDqIjJhqxzJsOpwltmp6jaTpeP_jgiHjslx0isrIjg8ZEUGjSqIB4RoMyWNX4daNYPbTYy6Wo7gaOk6sAmYkg5WGqd6giJkeoQpn11P5Eo9dkwLgmg2r6&lib=MwxUjRcLr2qLlnVOLh12wSNkqcO1Ikdrk');
-    http.Response response = await http.get(url);
-    Map data =  jsonDecode(response.body);
-    print('$data');
-
-  }*/
-
-  /*  void getData() async {
-    var url = Uri.parse('https://jsonplaceholder.typicode.com/todos/1');
-    http.Response response = await http.get(url);
-    Map data =  jsonDecode(response.body);
-    print(data);
-    print(data['title']);
-  }*/
   String time = 'loading';
 
   void setUpWordTime() async {
@@ -45,24 +30,26 @@ class _LoadingState extends State<Loading> {
         'time': instance.time,
       },
     );
-    /*print(instance.time);
-  setState(() {
-    time = instance.time;
-  });*/
   }
 
   @override
   void initState() {
     super.initState();
     setUpWordTime();
-    // getTime();
-    // getData();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(padding: const EdgeInsets.all(50.0), child: Text(time)),
+      backgroundColor: Colors.blue[900],
+      body: Center(
+       // child:  SpinKitRotatingCircle(
+       // child:  SpinKitCubeGrid(
+        child:  SpinKitFadingCube(
+          color: Colors.white,
+          size: 50.0,
+        ),
+      ),
     );
   }
 }
